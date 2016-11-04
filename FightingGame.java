8f2a743770d17ee792574f54dad255b824c51fc5
Salomon <salomonpluviose@yahoo.com>
2016-11-04 18:21:29 -0400

    added loop and switched turns for pokemon like fighting style

--- FightingGame.java
+++ FightingGame.java
@@ -21,17 +21,83 @@ public class FightingGame {
     }
 
     public void showMyMoves() {
+        int i = 0;
         for (String move : arsenal.keySet()) {
             System.out.println(characterName + " has " + move + " and it cases " + arsenal.get(move) + " damage.");
+            System.out.println("Choose " + i + " for " + arsenal.entrySet().toArray()[i].toString().split("=")[0]);
+            i++;
+        }
+
+//        System.out.println(arsenal);
+
+//        System.out.println(arsenal.entrySet().toArray()[0].toString().split("=")[0]);
+
+
+
+    }
+
+    public void idk(FightingGame fighter) {
+        System.out.println(characterName + " vs. " + fighter.characterName);
+        System.out.println("BEGIN");
+
+        showMyMoves();
+
+        System.out.println("Choose a move to use on you opponent");
+        Scanner choose = new Scanner(System.in);
+        int numberMove = choose.nextInt();
+
+        switch (numberMove) {
+            case 0: System.out.println();
         }
     }
 
-    public void useRandomPowerMoveOnFighter(FightingGame fighter) {
-        System.out.println(characterName + " " + arsenal.get(0) + "d " + fighter.characterName);
+    public void fight(FightingGame fighter) {
 
-        fighter.life -= arsenal.get(0);
+        do {
+            showMyMoves();
+            Scanner choose = new Scanner(System.in);
+            System.out.println(characterName + " choose a move!");
+            String playerOneMoveNumber = choose.next();
 
-        System.out.println(fighter.characterName + " is now " + fighter.life);
+            switch (playerOneMoveNumber) {
+                case "0": fighter.life = fighter.life - Integer.parseInt(arsenal.entrySet().toArray()[0].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + arsenal.entrySet().toArray()[0].toString().split("=")[0] + " and caused " + arsenal.entrySet().toArray()[0].toString().split("=")[1] + " damage. " + fighter.characterName + " now has " + fighter.life + " life left");
+                    break;
+                case "1": fighter.life = fighter.life - Integer.parseInt(arsenal.entrySet().toArray()[1].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + arsenal.entrySet().toArray()[1].toString().split("=")[0] + " and caused " + arsenal.entrySet().toArray()[1].toString().split("=")[1] + " damage. " + fighter.characterName + " now has " + fighter.life + " life left");
+                    break;
+                case "2": fighter.life = fighter.life - Integer.parseInt(arsenal.entrySet().toArray()[2].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + arsenal.entrySet().toArray()[2].toString().split("=")[0] + " and caused " + arsenal.entrySet().toArray()[2].toString().split("=")[1] + " damage. " + fighter.characterName + " now has " + fighter.life + " life left");
+                    break;
+                default: System.out.println("Incorrect key entered");
+                    break;
+            }
+
+            System.out.println(fighter.characterName + " choose a move!");
+            String playerTwoMoveNumber = choose.next();
+
+            fighter.showMyMoves();
+            switch (playerTwoMoveNumber) {
+                case "0": life = life - Integer.parseInt(fighter.arsenal.entrySet().toArray()[0].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + fighter.arsenal.entrySet().toArray()[0].toString().split("=")[0] + " and caused " + fighter.arsenal.entrySet().toArray()[0].toString().split("=")[1] + " damage. " + characterName + " now has " + life + " life left");
+                    break;
+                case "1": fighter.life = fighter.life - Integer.parseInt(fighter.arsenal.entrySet().toArray()[1].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + fighter.arsenal.entrySet().toArray()[1].toString().split("=")[0] + " and caused " + fighter.arsenal.entrySet().toArray()[1].toString().split("=")[1] + " damage. " + characterName + " now has " + life + " life left");
+                    break;
+                case "2": fighter.life = fighter.life - Integer.parseInt(fighter.arsenal.entrySet().toArray()[2].toString().split("=")[1]);
+                    System.out.println(characterName + " used " + fighter.arsenal.entrySet().toArray()[2].toString().split("=")[0] + " and caused " + fighter.arsenal.entrySet().toArray()[2].toString().split("=")[1] + " damage. " + characterName + " now has " + life + " life left");
+                    break;
+                default: System.out.println("Incorrect key entered");
+                    break;
+            }
+
+        } while (fighter.life <= 0 || life <= 0);
+
+        if (fighter.life <= 0) {
+            System.out.println(characterName + " wins!");
+        } else {
+            System.out.println(fighter.characterName + " wins!");
+        }
 
     }
 
@@ -48,11 +114,11 @@ public class FightingGame {
         kakashiMoveList.put("kamui", 200);
         FightingGame kakashi = new FightingGame("kakashi", kakashiMoveList, 400);
 
-        gokuu.showMyMoves();
-        kakashi.showMyMoves();
+        gokuu.fight(kakashi);
 
-        gokuu.useRandomPowerMoveOnFighter(kakashi);
-        kakashi.useRandomPowerMoveOnFighter(gokuu);
+
+//        gokuu.useRandomPowerMoveOnFighter(kakashi);
+//        kakashi.useRandomPowerMoveOnFighter(gokuu);
 
 
     }
